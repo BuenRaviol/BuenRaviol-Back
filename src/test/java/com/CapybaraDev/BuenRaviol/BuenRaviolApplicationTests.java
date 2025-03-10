4a9804c58199dbc2176ec7d4085e293270abccc3
Giuliano Espejo Mezzabotta <129322090+Giuliano-Espejo@users.noreply.github.com>
2025-03-10 17:27:36 -0300

    first commit

---
+++ src/test/java/com/CapybaraDev/BuenRaviol/BuenRaviolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CapybaraDev.BuenRaviol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuenRaviolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
