d6c9674d2fe8fb6836168a4740e181d81c28309d
Giuliano Espejo Mezzabotta <129322090+Giuliano-Espejo@users.noreply.github.com>
2025-03-25 18:56:40 -0300

    capa de repositorios

---
+++ src/main/java/com/entidades/buenSabor/repositories/ImagenProductoRepository.java
@@ -0,0 +1,8 @@
+package com.entidades.buenSabor.repositories;
+
+import com.entidades.buenSabor.domain.entities.ImagenProducto;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImagenProductoRepository extends BaseRepository<ImagenProducto,Long>{
+}
